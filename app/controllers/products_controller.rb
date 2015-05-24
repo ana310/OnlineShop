c23aa28138ae875afb7cf8fa9c75d10454eb454d
ana310 <ana.coman93@gmail.com>
2015-05-24 15:12:13 +0300

    adaugat lista channels, selectare products dupa channels s si adaptat view-ul pentru acestea

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @channels = Channel.all
+    if(params[:channel].blank?)
+      @products = Channel.find(1).products.all
+    else
+      @products = Channel.find(params[:channel]).products.all
+    end
   end
 end
